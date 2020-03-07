075ba2f6910b3866495bcdba40098b4afd616209
Aleksandr <strtmg@yandex.ru>
2020-03-08 06:44:24 +0700

    Setup project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YOUBIFY",
+    version="0.1",
+    packages=find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=[
+      'docutils',
+      'pandas',
+      'numpy',
+      'keras',
+      'tensorflow-cpu',
+      'turicreate'
+    ],
+)
